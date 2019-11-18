fc38f7fa6fc8b65d1aea459359065456bf441ab6
Oscar Hermosa <ohermosa@gmail.com>
2019-11-18 12:56:24 +0100

    correccion de errores deploy_vault

--- docker_deploys/vault/_scripts/setup.sh
+++ docker_deploys/vault/_scripts/setup.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+## FIX DATA PERMISSIONS
+echo "[*] Fix permissions for consul data directory..."
+sudo chmod 777 _data/consul
+docker restart consul
+sleep 5
+docker restart vault
+
 ## CONFIG LOCAL ENV
 echo "[*] Config local environment..."
 alias vault='docker-compose exec vault vault "$@"'
@@ -5,14 +14,18 @@ export VAULT_ADDR=https://127.0.0.1:8200
 
 ## INIT VAULT
 echo "[*] Init vault..."
-vault init -address=${VAULT_ADDR} > ./_data/keys.txt
-export VAULT_TOKEN=$(grep 'Initial Root Token:' ./_data/keys.txt | awk '{print substr($NF, 1, length($NF)-1)}')
+# vault init -address=${VAULT_ADDR} > ./_data/keys.txt
+curl -k -XPUT -d '{"secret_shares": 10,"secret_threshold": 5}' ${VAULT_ADDR}/v1/sys/init > ./_data/keys.txt
+export VAULT_TOKEN=$( cat _data/keys.txt | jq -r .root_token)
 
 ## UNSEAL VAULT
 echo "[*] Unseal vault..."
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 1:' ./_data/keys.txt | awk '{print $NF}')
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 2:' ./_data/keys.txt | awk '{print $NF}')
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 3:' ./_data/keys.txt | awk '{print $NF}')
+echo "{\"key\": \"$(cat _data/keys.txt | jq -r .keys_base64[1])\"}" > /tmp/key1.json
+echo "{\"key\": \"$(cat _data/keys.txt | jq -r .keys_base64[2])\"}" > /tmp/key2.json
+echo "{\"key\": \"$(cat _data/keys.txt | jq -r .keys_base64[3])\"}" > /tmp/key3.json
+curl -k -X PUT -d@/tmp/key1.json ${VAULT_ADDR}/v1/sys/unseal
+curl -k -X PUT -d@/tmp/key2.json ${VAULT_ADDR}/v1/sys/unseal
+curl -k -X PUT -d@/tmp/key3.json ${VAULT_ADDR}/v1/sys/unseal
 
 ## AUTH
 echo "[*] Auth..."
