97a464f183c17bccd63b8db6b7e71725b5e6aabb
Oscar Hermosa <ohermosa@gmail.com>
2020-03-09 14:49:00 +0100

    nuevo modulo gitlab_get_repos

---
+++ ansible/library/gitlab_get_repos.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+import requests
+import json
+
+DOCUMENTATION = '''
+---
+module: gitlab_get_repos
+short_description: Manage your repos on gitlab
+'''
+
+EXAMPLES = '''
+- name: get repos of gitlab repository
+  gitlab_get_repos:
+    gitlab_auth_key: "..."
+  register: result
+
+- name: get forked repositories
+  debug: var=result.repos
+'''
+
+api_url = "https://gitlab.com/api/v4"
+
+
+def gitlab_get_user_id(data=None):
+    headers = {
+        "PRIVATE-TOKEN": "{}" . format(data['gitlab_auth_key'])
+    }
+    url = "{}/user" . format(api_url)
+    result = requests.get(url, headers=headers)
+
+    if result.status_code == 200:
+        return result.json()["username"]
+    else:
+        return None
+
+
+def gitlab_get_project_id(data=None):
+    headers = {
+        "PRIVATE-TOKEN": "{}" . format(data['gitlab_auth_key'])
+    }
+    url = "{}/users/{}/projects?pagination=keyset&per_page=99&order_by=id" . format(api_url, gitlab_get_user_id(data))
+    result = requests.get(url, headers=headers)
+
+    if result.status_code == 200:
+        for i in result.json():
+            if i["name"] == data["name"]:
+                return i["id"]
+        return None
+    else:
+        return None
+
+
+def get_repos(token):
+    data = {
+        "gitlab_auth_key": token
+    }
+
+    headers = {
+        "PRIVATE-TOKEN": "{}" . format(token)
+    }
+    url = "{}/projects/{}/" . format(api_url, gitlab_get_project_id(data))
+    result = requests.get(url, headers=headers)
+    who_am_i = gitlab_get_user_id(data)
+
+    list = []
+    if result.status_code == 200:
+        for fork in result.json():
+            list.append({
+                        "repo": fork["owner"]["username"],
+                        "repo_url": "git@gitlab.com:{}/{}.git".format(who_am_i)
+                    })
+        return list
+    else:
+        return []
+
+
+    if result.status_code == 202:
+        return False, True, {"status": "SUCCESS"}
+    if result.status_code == 404:
+        result = {"status": result.status_code, "data": result.json()}
+        return False, False, result
+    else:
+        result = {"status": result.status_code, "data": result.json()}
+        return True, False, result
+
+
+def main():
+    module_args = dict(
+        gitlab_auth_key=dict(type='str', required=True)
+    )
+
+    result = dict(
+        changed=False,
+        repos=list
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    result['repos'] = get_repos(token=module.params["gitlab_auth_key"])
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
